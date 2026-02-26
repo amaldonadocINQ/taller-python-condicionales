e72a4ba3a7e342d2447774f9e6d00e0e57d3c4d6
Amelia Maldonado Carrillo <amaldonadoc@estud.usfq.edu.ec>
2026-02-26 07:36:10 -0500

    Pide al usuario su nombre, edad y ciudad, para luego imprimir en un mensaje completo

---
+++ saludoFormateado.py
@@ -0,0 +1,4 @@
+nombre = input("Ingresa tu nombre: ")
+edad = int(input("Ingresa tu edad: "))
+ciudad = input("Ingresa la ciudad en donde vives: ")
+print(f"Hola, {nombre}, tienes {edad}años y vives en {ciudad}")
