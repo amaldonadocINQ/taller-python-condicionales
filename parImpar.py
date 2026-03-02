cefa093bc4645c329a352a76e2f9687b865d1567
amaldonadocINQ <amaldonadoc@estud.usfq.edu.ec>
2026-03-01 19:40:20 -0500

    Programa que pide un número y muestra si es par o impar. Además ayuda a clasificar tu edad en mayor o menor de edad

--- parImpar.py
+++ parImpar.py
@@ -1,5 +1,10 @@
 numero = int(input("Porfavor ingresa un número: "))
+edad = int(input("Porfavor ingresa tu edad: "))
 if numero%2 == 0:
     print ("El número es par")
 else:
     print ("El número es impar")
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
